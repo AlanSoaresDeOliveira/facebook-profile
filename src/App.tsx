0818f0f13904db277428d78b60b6ffd471b53fca
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-07-17 10:04:44 -0300

    Created the basic structure to the application

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Profile from './pages/Profile';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
